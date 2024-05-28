fbe9a365942652ca631ab69d61022a380d7fa285
aaungdev <dhev.code@gmail.com>
2024-05-27 22:15:13 -0400

    added function for cin

--- project01.cpp
+++ project01.cpp
@@ -15,6 +15,31 @@ struct Student {
 Student students[MAX_STUDENTS];
 int studentCount = 0;
 
+// Function to insert a new student record
+void insertStudent() {
+    if (studentCount >= MAX_STUDENTS) {
+        cout << "Student record list is full." << endl;
+        return;
+    }
+
+    cout << "Enter Student ID: ";
+    cin >> students[studentCount].id;
+    cin.ignore(); // to ignore the newline character left by cin
+
+    cout << "Enter Student Name: ";
+    cin.getline(students[studentCount].name, MAX_NAME_LENGTH);
+
+    cout << "Enter Student Age: ";
+    cin >> students[studentCount].age;
+    cin.ignore();
+
+    cout << "Enter Student Course: ";
+    cin.getline(students[studentCount].course, MAX_COURSE_LENGTH);
+
+    studentCount++;
+    cout << "Student record inserted successfully." << endl;
+}
+
 int main() {
     return 0;
 }
